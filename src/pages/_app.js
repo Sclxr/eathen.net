6f66ae9eef1d1455e444538db2f3fe258dda0919
Eathen <129014091+Sclxr@users.noreply.github.com>
2026-03-01 23:40:05 -0500

    Added Google Analytics and new detailing images

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
- import "../styles/globals.css";
+import "../styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,23 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZJS9K9T97S"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZJS9K9T97S', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );

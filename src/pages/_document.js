0d04cb5676ce82a94831cd73f7e2462749fc8a58
Eathen <129014091+Sclxr@users.noreply.github.com>
2026-03-01 23:46:36 -0500

    Update deployed site

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,30 @@
 // src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GA_ID = "G-ZJS9K9T97S";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Favicon */}
         <link rel="icon" href="/E.png" />
 
-        {/* Basics */}
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#0f1419" />
         <meta name="color-scheme" content="dark light" />
 
-        {/* Optional Manifest */}
-        {/* <link rel="manifest" href="/site.webmanifest" /> */}
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { send_page_view: true });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
